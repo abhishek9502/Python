51be5b50ff74f8d286565659852fc53b7212e8ae
Abhishek Heerekar <abhishek.heerekar@acuvate.com>
2026-08-20 12:50:05 +0530

    updated code

--- firstPattern.py
+++ firstPattern.py
@@ -1,7 +1,3 @@
 n=int(input())
 for i in range(1,n+1):
-    for j in range(1,n-i+1):
-        print(" ",end="")
-    for j in range(1,n+1):
-        print("$",end="")
-    print()
+    print(" "*(n-i)+"$"*n)
